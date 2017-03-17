4632b30a38ae5b4c3ea802906b572019f318340a
최은비 <ebchoi@bulkytree.com>
2017-03-17 16:12:17 +0900

    아이디 중복 방지

--- routes/auth/signUp.js
+++ routes/auth/signUp.js
@@ -1,5 +1,8 @@
 "use strict"
 
+const
+    async = require('async');
+
 function init(app) {
     app.get('/signUp', function(req, res) {
         res.render('auth/signUp');
@@ -9,12 +12,14 @@ function init(app) {
         let user_id = req.body.user_id;
         let user_name = req.body.user_name;
         let user_pw = req.body.user_pw;
-
+    
         req.cache.hset('user', user_id, user_pw, (err, result) => {
-            if (err) throw err;
+                if (err) throw err;
+                if (result == 0) 
+                    return res.send({result: 0});
 
-            console.log('회원 가입 성공');
-            res.send({result: 1});
+                console.log('회원 가입 성공');
+                return res.send({result: 1});
         });
     });
 }
