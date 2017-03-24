a50e1b0ac98bae0b38937efa584dfdebf290b9e5
최은비 <ebchoi@bulkytree.com>
2017-03-24 17:02:08 +0900

    패스워드 해싱 bcrypt 사용

--- routes/auth/login.js
+++ routes/auth/login.js
@@ -2,6 +2,8 @@
 
 const
     async = require('async'),
+    bcrypt = require('bcrypt'),
+
     lib = require('../../lib/lib');
 
 let sessionMap = lib.getSessionMap();
@@ -11,66 +13,54 @@ function init(app) {
 
     app.get('/login', function (req, res) {
         prev_path = req.query.prev_path;
-
         res.render('auth/login');
     });
 
+    /**
+     * TODO.
+     * 클라이언트단에서 패스워드가 평문으로 오기 때문에
+     * 무조건 https로 바꿔줘야 한다.
+     */
     app.post('/login', function (req, res) {
         let input_id = req.body.input_id;
         let input_pw = req.body.input_pw;
 
-        let user_name;
+        let multi = req.cache.multi();
 
-        async.waterfall([
-            (callback) => {
-                req.cache.hexists('user', input_id, (err, result) => { // 아이디 맞는지 확인
-                    if (err) callback (err);
+        multi.hget('user', input_id);
+        multi.hget('ID::' + input_id, 'name');
+        multi.exec(function (err, results) {
+            if (err) throw err;
 
-                    if (result != 1)
-                        return res.render('auth/login', { error: 'Wrong Id!' });
+            let hash = results[0];
+            let user_name = results[1];
 
-                    callback();
-                })
-            },
-            (callback) => {
-                req.cache.hget('user', input_id, (err, result) => { // 비밀번호 확인
-                    if (err) callback(err);
+            if (!hash || !user_name) return res.render('auth/login', { error: 'Wrong Id or Password!' });
 
-                    if (input_pw != result)
-                        return res.render('auth/login', { error: 'Wrong password!' });
+            bcrypt.compare(input_pw, hash, function (err, result) {
+                if (err) throw err;
+                if (!result) return res.render('auth/login', { error: 'Wrong Id or Password!' });
 
-                    callback();
-                });
-            },
-            (callback) => {
-                req.cache.hget('ID::' + input_id, 'name', (err, result) => { // 해당 유저 정보 읽기
-                    if (err) callback(err);
-
-                    user_name = result;
-
-                    callback();
-                });
-            }
-        ], (err, result) => {
-            if (sessionMap.hasOwnProperty(input_id))
+                if (sessionMap.hasOwnProperty(input_id))
                 delete sessionMap[input_id];
 
-            let session_key = lib.getRandomNum();
-            sessionMap[input_id] = session_key;
-            
-            console.log('sessionMap : ', sessionMap);
+                let session_key = lib.getRandomNum();
+                sessionMap[input_id] = session_key;
 
-            req.session.userData = {
-                'user_id': input_id,
-                'user_name': user_name,
-                'session_key': session_key
-            }
+                console.log('sessionMap : ', sessionMap);
 
-            if (prev_path == '' || prev_path == 'undefined' || prev_path == null)
-                res.redirect('/menu');
-            else
-                res.redirect(prev_path);
-        });
+                req.session.userData = {
+                    'user_id': input_id,
+                    'user_name': user_name,
+                    'session_key': session_key
+                }
+
+                if (prev_path == '' || prev_path == 'undefined' || prev_path == null)
+                    res.redirect('/menu');
+                else
+                    res.redirect(prev_path);
+            });
+        }); 
     });
 }
 exports.init = init;
--- routes/auth/signUp.js
+++ routes/auth/signUp.js
@@ -1,7 +1,10 @@
 "use strict"
 
 const
-    async = require('async');
+    async = require('async'),
+    bcrypt = require('bcrypt');
+const
+    saltRounds = 12;
 
 function init(app) {
     app.get('/signUp', function(req, res) {
@@ -13,20 +16,24 @@ function init(app) {
         let user_name = req.body.user_name;
         let user_email = req.body.user_email;
         let user_pw = req.body.user_pw;
-        
-        req.cache.hexists('user', user_id, (err, result) => {
+
+        bcrypt.hash(user_pw, saltRounds, function (err, hash) {
             if (err) throw err;
-            if (result == 1) return res.send({'result': -1}); //동일한 아이디 이미 존재
 
-            let multi = req.cache.multi();
-            multi.hset('user', user_id, user_pw);
-            multi.hmset('ID::' + user_id, 'name', user_name, 'email', user_email);
-            multi.exec(function (err, results) {
+            req.cache.hexists('user', user_id, (err, result) => {
                 if (err) throw err;
+                if (result == 1) return res.send({'result': -1}); //동일한 아이디 이미 존재
 
-                return res.send({'result': 1});
+                let multi = req.cache.multi();
+                multi.hset('user', user_id, hash);
+                multi.hmset('ID::' + user_id, 'name', user_name, 'email', user_email);
+                multi.exec(function (err) {
+                    if (err) throw err;
+
+                    return res.send({'result': 1});
+                });
             });
-        });
+        });        
     });
 }
 exports.init = init;
