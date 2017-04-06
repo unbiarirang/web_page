7fb6290a86aeb69ded2d265166fcef8c7d54e7a8
최은비 <ebchoi@bulkytree.com>
2017-04-06 18:31:18 +0900

    메세지 턴으로 주고받기 성공

--- routes/chat/chat.js
+++ routes/chat/chat.js
@@ -1,6 +1,11 @@
 "use strict"
 const
 	lib = require('../../lib/lib');
+const
+	STANDBY = 100, //TODO 모듈화
+	READY = 200,
+	PLAY = 300,
+	FINISH = 400;
 
 let rooms = require('./socket').rooms;
 
@@ -21,7 +26,7 @@ function init (app) {
 		let sendData = {};
 		sendData.rooms = rooms;
 		sendData.last_room_id = req.cookies.last_room_id;
-		
+
 		res.send(sendData);
 	});
 
@@ -30,11 +35,12 @@ function init (app) {
 			let sendData = {};
 			let user_name = req.session.userData.user_name;
 			let room_id = req.params.room_id;
+			let isResume = req.query.isResume;
 			let new_room_id;
 
-			if (room_id == 'match') {
+			if (room_id == 'match') { 
 				for (let key in rooms) {
-					if (rooms[key].userlist.length < 2) {
+					if (rooms[key].userlist.length < 2 && rooms[key].play_status <= STANDBY) {
 						new_room_id = key;
 						break;
 					}
@@ -52,20 +58,30 @@ function init (app) {
 
 			let room = rooms[room_id];
 
-			// if (room && room.userlist.indexOf(user_name) > -1)	//TODO 중복 입장 방지인데 새로고침을 허용해야됨?
-			// 	return res.redirect('/chat');
+			// if (room && room.userlist.length >= 2) //방 인원 꽉 참 (클라서도 막음)
+			// 	return res.redirect('/chat?fail_reason=The room is full'); //TODO 관리자일 때는 입장 가능하게 해줘야함.
 
-			if (room && room.userlist.length >= 2) 	//인원 꽉 참
-				return res.redirect('/chat'); 		//TODO 관리자일 때는 입장 가능하게 해줘야함.
+			if (room && room.play_status >= PLAY && req.cookies.last_room_id != room_id) //게임이 진행중인 방. 튕긴사람은 들어갈 수 있게 (클라서도 막음)
+				return res.redirect('/chat?fail_reason=Game is ongoing'); //TODO 관리자일 때는 입장 가능하게 해줘야함.
+
+			if (isResume && !rooms[room_id]) //'이전 방 들어가기' 누름. 하지만 방이 이미 사라져서 못들어감. (클라서도 막음)
+				return res.redirect('/chat?fail_reason=The room does not exist any more');
 
 			sendData.room_id = room_id;
 			sendData.rooms = rooms;
 			sendData.user_name = user_name;
 
-			res.cookie('last_room_id', room_id, { maxAge: 1 * 60 * 1000, httpOnly: true}); //채팅방 정보는 1분만 쿠키에 보관. TODO 나갈때 보관해야되는데....
+			res.cookie('last_room_id', room_id, { maxAge: 10 * 60 * 1000, httpOnly: true}); //last_room_id 10분간 쿠키에 저장
 
 			res.render('chat/chatRoom', sendData);
 		});
+	});
+
+	app.post('/chat/:room_id/control', function (req, res) {
+		let value = req.body.value;
+		console.log('value: ', value);
+
+		res.send({'resultData': 'ok'});
 	});
 }
 exports.init = init;
--- routes/chat/socket.js
+++ routes/chat/socket.js
@@ -1,5 +1,11 @@
 "use strict"
 
+const
+	STANDBY = 100,
+	READY = 200,
+	PLAY = 300,
+	FINISH = 400;
+
 let rooms = {};
 let userlist = {};
 
@@ -23,7 +29,7 @@ function init(http) {
 			io.sockets.in('lobby').emit('lobbyChat', socket.user_name + ': ' + msg);
 		});
 
-		socket.on('adduser', function (data) {
+		socket.on('addUser', function (data) {
 			let user_name = data.user_name;
 			let room_id = data.room_id;
 
@@ -31,8 +37,11 @@ function init(http) {
 			socket.room_id = room_id;
 
 			if (!rooms.hasOwnProperty(room_id)) {       //새로운방 생성
-				let room = {};
-				room.userlist = [];
+				let room = {
+					'play_status' : STANDBY,
+					'userlist' : [],
+					'who_ready' : ''
+				}
 				room.userlist.push(user_name);
 				rooms[room_id] = room;
 				userlist[user_name] = room_id;
@@ -50,17 +59,50 @@ function init(http) {
 		});
 
 		socket.on('chat', function (msg) {
-			console.log('roomId ', socket.room_id, ' message from ' + socket.user_name + ': ' + msg);
-			io.sockets.in(socket.room_id).emit('chat', socket.user_name + ': ' + msg);
+			let room_id = socket.room_id;
+			let user_name = socket.user_name;
+
+			console.log('roomId ', room_id, ' message from ' + user_name + ': ' + msg);
+			io.sockets.in(room_id).emit('chat', user_name + ': ' + msg);
+		});
+
+		socket.on('ready', function (data) {
+			let room_id = socket.room_id;
+			let user_name = socket.user_name;
+			let play_status = rooms[room_id].play_status;
+
+			console.log('rooms: ', rooms);
+
+			if (rooms[room_id].userlist.length == 1) return; 		//혼자 있을 때 레디
+			if (play_status >= PLAY) return;						//이미 플레이 중
+			if (rooms[room_id].who_ready == data.user_name) return; //한사람이 레디 여러번함.
+
+			if (play_status == READY) {
+				rooms[room_id].play_status = PLAY;
+				io.sockets.in(room_id).emit('chat', 'SERVER: GAME START');
+				socket.broadcast.to(room_id).emit('myTurn');
+				return;
+			}
+
+			console.log(socket.user_name + ' is ready');
+			rooms[room_id].play_status = READY;
+			rooms[room_id].who_ready = data.user_name;
+			io.sockets.in(room_id).emit('chat', 'SERVER: ' + user_name + ' IS READY~');
+		});
+
+		socket.on('yourTurn', function () {
+			let room_id = socket.room_id;
+
+			console.log('in yourTurn ');
+			socket.broadcast.to(room_id).emit('myTurn');
 		});
 
 		socket.on('disconnect', function () {
 			let user_name = socket.user_name;
 			let room_id = socket.room_id;
 
-			if (userlist[user_name] == 'lobby') { //로비에서 나감
+			if (userlist[user_name] == 'lobby') //로비에서 나감
 				return delete userlist[user_name]; 
-			}
 
 			if (!rooms[room_id] || rooms[room_id].userlist.indexOf(user_name) < 0) //이미 삭제된 룸이거나 이미 삭제된 유저
 				return;
@@ -72,11 +114,20 @@ function init(http) {
 			else {                                          //채팅방 유저 리스트에서 자신 삭제
 				let index = rooms[room_id].userlist.indexOf(user_name);
 				rooms[room_id].userlist.splice(index, 1);
+				
+				if (rooms[room_id].play_status < PLAY) {	//게임 도중에 나온게 아니라면 방 부분 초기화
+					rooms[room_id].play_status = STANDBY;
+					rooms[room_id].who_ready = '';
+				}
 			}
 
 			console.log('rooms:', rooms, '\n userlist: ', userlist);
 			socket.broadcast.to(room_id).emit('chat', 'SERVER: ' + user_name + ' 님이 퇴장하셨습니당.');
-			socket.broadcast.to(room_id).emit('terminate', 'SERVER: 60초 후에 자동 퇴장됩니다.');
+
+			if (rooms[room_id] && rooms[room_id].play_status == PLAY)
+				socket.broadcast.to(room_id).emit('terminate', PLAY);
+			if (rooms[room_id] && rooms[room_id].play_status == FINISH)
+				socket.broadcast.to(room_id).emit('terminate', FINISH);
 		});
 	});
 }
